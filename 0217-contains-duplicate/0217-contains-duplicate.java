981a7d3a966d53abfea91ca758eb313f1a82b125
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-02-24 10:14:13 +0530

    Time: 10 ms (88.39%), Space: 58.1 MB (52.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -9,15 +9,15 @@ class Solution {
             set.add(n);   
         }
         return false;
-        //=============================================
-        // Map Approach
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            if(map.containsKey(n) && map.get(n) >= 1){
-                return true;
-            }
-            map.put(n, map.getOrDefault(n,0)+1);
-        }
-        return false;
+        //=========================================
+        // /* Map Approach */
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int n: nums){
+        //     if(map.containsKey(n) && map.get(n) >= 1){
+        //         return true;
+        //     }
+        //     map.put(n, map.getOrDefault(n,0) + 1);
+        // }
+        // return false;
     }
 }
