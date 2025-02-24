998d6138f206aab6a102a4905961633990d55201
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-02-24 09:53:30 +0530

    Update 0217-contains-duplicate.java

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        // Set Approach
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);   
+        }
+        return false;
+        //=============================================
         // Map Approach
         Map<Integer, Integer> map = new HashMap<>();
         for(int n: nums){
