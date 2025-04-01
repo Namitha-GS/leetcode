b01b43cf3b5fb99fec95f3ad001c1729faabb4fb
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-04-01 10:19:39 +0530

    Time: 0 ms (100%), Space: 41.7 MB (25.57%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,35 +1,65 @@
 class MyStack {
 
-    Queue<Integer> q1;
-    Queue<Integer> q2;
+    Queue<Integer> q;
 
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q = new LinkedList<>();
     }
     
     public void push(int x) {
-        while(q1.size() > 0) {
-            q2.add(q1.remove());
-        }
-        q1.add(x);
-        while(q2.size() > 0) {
-            q1.add(q2.remove());
+        q.add(x);
+        for(int i=0; i<q.size()-1; i++) {
+            q.add(q.remove());
         }
     }
     
     public int pop() {
-        return q1.remove();
+       return q.remove();
     }
     
     public int top() {
-       return q1.peek(); 
+       return q.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return q.isEmpty();
     }
 }
+
+/*
+* By using 2 Queues
+*/
+//  class MyStack {
+//     Queue<Integer> q1;
+//     Queue<Integer> q2;
+
+//     public MyStack() {
+//         q1 = new LinkedList<>();
+//         q2 = new LinkedList<>();
+//     }
+    
+//     public void push(int x) {
+//         while(q1.size() > 0) {
+//             q2.add(q1.remove());
+//         }
+//         q1.add(x);
+//         while(q2.size() > 0) {
+//             q1.add(q2.remove());
+//         }
+//     }
+    
+//     public int pop() {
+//         return q1.remove();
+//     }
+    
+//     public int top() {
+//        return q1.peek(); 
+//     }
+    
+//     public boolean empty() {
+//         return q1.isEmpty();
+//     }
+// }
 
 /**
  * Your MyStack object will be instantiated and called as such:
