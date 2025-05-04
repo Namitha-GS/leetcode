0e662ca5864d95df617305e8c6f9145b25788ddc
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-04 20:41:21 +0530

    Time: 0 ms (100%), Space: 41 MB (92.46%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,22 +1,40 @@
 class Solution {
+    /*
+    * without using Queue
+    */
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<tickets.length; i++) {
-            q.add(i);
-        }
         int time = 0;
-        while(!q.isEmpty()) {
-            int id = q.poll();
-            time++;
-            tickets[id]--;
-            if(id == k && tickets[id] == 0) {
-                return time;
+        for(int i=0; i<tickets.length; i++) {
+            if(i <= k) {
+                time += Math.min(tickets[k], tickets[i]);
             }
-            if(tickets[id] != 0) {
-                q.add(id);
+            else {
+                time += Math.min(tickets[k]-1, tickets[i]);
             }
         }
         return time;
-        
     }
+
+    /*
+    * using Queue
+    */
+    // public int timeRequiredToBuy(int[] tickets, int k) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     for(int i=0; i<tickets.length; i++) {
+    //         q.add(i);
+    //     }
+    //     int time = 0;
+    //     while(!q.isEmpty()) {
+    //         int id = q.poll();
+    //         time++;
+    //         tickets[id]--;
+    //         if(id == k && tickets[id] == 0) {
+    //             return time;
+    //         }
+    //         if(tickets[id] != 0) {
+    //             q.add(id);
+    //         }
+    //     }
+    //     return time;
+    // }
 }
