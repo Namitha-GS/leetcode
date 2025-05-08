8a2353587de54b44b9caf23438fe2d199325f5ee
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-08 06:17:30 +0530

    Time: 111 ms (27.96%), Space: 57.6 MB (43.76%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,47 +1,45 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int maxArea=0;
-        int nsr[] = new int[heights.length];
-        int nsl[] = new int[heights.length];
+    public int largestRectangleArea(int[] arr) {
+        int n = arr.length;
+        int maxarea = 0;
+        int[] nsr = new int[n];
+        int[] nsl = new int[n];
+        Stack<Integer> s = new Stack<>();
 
-
-        //next smaller right
-        Stack<Integer> s= new Stack<>();
-        for(int i=heights.length-1;i>=0;i--){
-            while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
+        // to find NSE right
+        for(int i=arr.length-1; i>=0; i--) {
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]) {
                 s.pop();
             }
-            if(s.isEmpty()){
-                nsr[i]=heights.length;
-            }else{
-                nsr[i]=s.peek();
+            if(s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
             }
             s.push(i);
         }
 
-        //next smaller left
-        s =new Stack<>();
-        for(int i=0;i<heights.length;i++){
-            while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
+        // to find NSE left
+        s = new Stack<>();
+        for(int i=0; i<arr.length; i++) {
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]) {
                 s.pop();
             }
-            if(s.isEmpty()){
-                nsl[i]=-1;
-            }else{
-                nsl[i]=s.peek();
+            if(s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
             }
             s.push(i);
         }
 
-        //Current Area:width = j-i-1 = nsr[i]-nsl[i]-1
-        for(int i=0;i<heights.length;i++){
-            int height=heights[i];
-            int width = nsr[i]-nsl[i]-1;
-            int currArea = height*width;
-            maxArea=Math.max(currArea, maxArea);
-
+        // to find area
+        for(int i=0; i<arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int area = height * width;
+            maxarea = Math.max(area, maxarea);
         }
-        return maxArea;
-
+        return maxarea;
     }
 }
