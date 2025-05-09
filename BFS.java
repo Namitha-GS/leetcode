99f367bbaa4f1b1155da32f3f08b5d956ce6dc4b
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-09 20:14:34 +0530

    Breadth-First Search

--- BFS.java
+++ BFS.java
@@ -25,14 +25,14 @@ public class BFS {
 		List<List<Integer>> adj = new ArrayList<>();
 		adj.add(new ArrayList<>());
 		adj.add(new ArrayList<>(Arrays.asList(2,6)));
-    adj.add(new ArrayList<>(Arrays.asList(1,3,4)));
-    adj.add(new ArrayList<>(Arrays.asList(2)));   
-    adj.add(new ArrayList<>(Arrays.asList(2,5)));
-    adj.add(new ArrayList<>(Arrays.asList(4,8)));
-    adj.add(new ArrayList<>(Arrays.asList(1,7,9)));
-    adj.add(new ArrayList<>(Arrays.asList(6,8)));
-    adj.add(new ArrayList<>(Arrays.asList(7,5)));
-    adj.add(new ArrayList<>(Arrays.asList(6)));
+		adj.add(new ArrayList<>(Arrays.asList(1,3,4)));
+		adj.add(new ArrayList<>(Arrays.asList(2)));
+		adj.add(new ArrayList<>(Arrays.asList(2,5)));
+		adj.add(new ArrayList<>(Arrays.asList(4,8)));
+		adj.add(new ArrayList<>(Arrays.asList(1,7,9)));
+		adj.add(new ArrayList<>(Arrays.asList(6,8)));
+		adj.add(new ArrayList<>(Arrays.asList(7,5)));
+		adj.add(new ArrayList<>(Arrays.asList(6)));
 		List<Integer> list = getBFS(adj, adj.size());
 		System.out.println(list);
 	}
