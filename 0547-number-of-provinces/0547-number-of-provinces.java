e42ae580e6940cbc80b01904125aa327c207efc3
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-11 21:28:55 +0530

    Time: 1 ms (88.84%), Space: 48.2 MB (29.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,27 +1,64 @@
 class Solution {
-
-    private static void dfs(int node, int[][] arr, boolean[] visited) {
+    /*
+    * using BFS
+    */
+    private static void bfs(int node, int[][] arr, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
         visited[node] = true;
 
-        // find the neighboring nodes whether its visited
-        for(int i=0; i<arr.length; i++) {
-            if(arr[node][i] == 1 && !visited[i]) {
-                dfs(i, arr, visited);
+        while(!q.isEmpty()) {
+            int n = q.poll();
+            for(int i=0; i<arr.length; i++) {
+                if(arr[n][i] == 1 && !visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int provinceCnt = 0;
+        int cnt = 0;
         boolean[] visited = new boolean[n];
-        // check what all nodes are not yet visited
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                provinceCnt++;
-                dfs(i, isConnected, visited);
+                cnt++;
+                bfs(i, isConnected, visited);
             }
         }
-        return provinceCnt;
+        return cnt;
     }
 }
+
+
+/*
+* using DFS
+*/
+// class Solution {
+
+//     private static void dfs(int node, int[][] arr, boolean[] visited) {
+//         visited[node] = true;
+//         // find the neighboring nodes whether its visited
+//         for(int i=0; i<arr.length; i++) {
+//             if(arr[node][i] == 1 && !visited[i]) {
+//                 dfs(i, arr, visited);
+//             }
+//         }
+//     }
+
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         int provinceCnt = 0;
+//         boolean[] visited = new boolean[n];
+//         // check what all nodes are not yet visited
+//         for(int i=0; i<n; i++) {
+//             if(!visited[i]) {
+//                 provinceCnt++;
+//                 dfs(i, isConnected, visited);
+//             }
+//         }
+//         return provinceCnt;
+//     }
+// }
