7daf598586562a2e15fd4f2648d5ef1ef4faf62a
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-11 07:01:13 +0530

    Depth-First Search

--- DFS.java
+++ DFS.java
@@ -23,15 +23,15 @@ public class DFS {
 		List<List<Integer>> adj = new ArrayList<>();
 		adj.add(new ArrayList<>());
 		adj.add(new ArrayList<>(Arrays.asList(2,3)));
-    adj.add(new ArrayList<>(Arrays.asList(1,5,6)));
-    adj.add(new ArrayList<>(Arrays.asList(1,4,7)));
-    adj.add(new ArrayList<>(Arrays.asList(3,8)));
-    adj.add(new ArrayList<>(Arrays.asList(2)));
-    adj.add(new ArrayList<>(Arrays.asList(2)));
-    adj.add(new ArrayList<>(Arrays.asList(3,8)));
-    adj.add(new ArrayList<>(Arrays.asList(4,7)));
-    List<Integer> l = getDFS(adj);
-    System.out.println(l);
+		adj.add(new ArrayList<>(Arrays.asList(1,5,6)));
+		adj.add(new ArrayList<>(Arrays.asList(1,4,7)));
+		adj.add(new ArrayList<>(Arrays.asList(3,8)));
+		adj.add(new ArrayList<>(Arrays.asList(2)));
+		adj.add(new ArrayList<>(Arrays.asList(2)));
+		adj.add(new ArrayList<>(Arrays.asList(3,8)));
+		adj.add(new ArrayList<>(Arrays.asList(4,7)));
+		List<Integer> l = getDFS(adj);
+		System.out.println(l);
 	}
 
 }
