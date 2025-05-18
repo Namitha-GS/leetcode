6a8da89c91a7aa12e34f908f66ab20a05819565d
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-05-18 15:39:40 +0530

    Time: 6 ms (29.78%), Space: 45.2 MB (38.9%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,17 +9,28 @@
  */
 
 class Solution {
-    // Iterative
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while(root != null) {
-            if(p.val < root.val && q.val < root.val) {
-                root = root.left;
-            } else if(p.val > root.val && q.val > root.val) {
-                root = root.right;
-            } else {
-                return root;
-            }
-        }
-        return null;
+       /* Recursive */
+       if(p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q);
+       } else if(p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+       } else {
+        return root;
+       }
     }
+
+    // /* Iterative */
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     while(root != null) {
+    //         if(p.val < root.val && q.val < root.val) {
+    //             root = root.left;
+    //         } else if(p.val > root.val && q.val > root.val) {
+    //             root = root.right;
+    //         } else {
+    //             return root;
+    //         }
+    //     }
+    //     return null;
+    // }
 }
