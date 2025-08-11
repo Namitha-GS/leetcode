c304f192165dda0d8e27370bef45a27809779da6
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2025-08-11 06:37:51 +0530

    Time: 5 ms (32.01%), Space: 52.4 MB (16.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,5 +1,52 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
+       if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int cnt = 0;
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    cnt++;
+                    bfs(grid, i, j);
+                }
+            }
+        } 
+        return cnt;
+    }
+
+    public void bfs(char[][] grid, int i, int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i,j});
+        grid[i][j] = '0';
+
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()) {
+            int[] cell = q.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+            for(int[] d : directions) {
+                int newrow = row + d[0];
+                int newcol = col + d[1];
+
+                if(newrow >=0 && newrow < grid.length
+                   && newcol >=0 && newcol <grid[0].length
+                   && grid[newrow][newcol] == '1') {
+                    q.offer(new int[]{newrow, newcol});
+                    grid[newrow][newcol] = '0';
+                   }
+            }
+        }
+    }
+
+
+
+
+
+    public int numIslandsDFS(char[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
